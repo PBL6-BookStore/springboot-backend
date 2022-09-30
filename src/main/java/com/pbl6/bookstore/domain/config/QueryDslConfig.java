82e202c78d9be175313bdd15520e8dd53060391e
Le Khac Anh Dai <khacdai0801@gmail.com>
2022-10-01 00:09:57 +0700

    revise

--- src/main/java/com/pbl6/bookstore/domain/config/QueryDslConfig.java
+++ src/main/java/com/pbl6/bookstore/domain/config/QueryDslConfig.java
@@ -1,8 +1,11 @@
 package com.pbl6.bookstore.domain.config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author lkadai0801
  * @since 30/09/2022
  */
+@Configuration
 public class QueryDslConfig {
 }
